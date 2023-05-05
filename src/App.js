2da46899e3024dbb4ed8c4da09221c143b2a222a
ameerparial <khuhroameerhamza4@gmail.com>
2023-05-05 10:21:35 +0500

    Three functionality added: increase count, decrease count, reset count on button click event.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import MyButton from './Components/MyButton';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  function increaseValue(){
+    setCount(count+1);
+  }
+  function resetValue(){
+    setCount(0);
+  }
+  function decreaseValue(){
+    setCount(count-1);
+  }
   return (
-    <MyButton/>
+    <div className='App'>
+      <MyButton count={count} increaseCount={increaseValue} resetCount={resetValue} decreaseCount={decreaseValue}/>
+    </div>
   );
 }
 
--- src/Components/MyButton.js
+++ src/Components/MyButton.js
@@ -1,4 +1,9 @@
-function MyButton(){
-    return <button>i'm button</button>
+function MyButton(props){
+    return <>
+        <h1>{props.count}</h1>
+        <button onClick={props.increaseCount}>count</button>
+        <button onClick={props.decreaseCount}>decrease</button>
+        <button onClick={props.resetCount}>reset</button>        
+    </>
 }
 export default MyButton;
