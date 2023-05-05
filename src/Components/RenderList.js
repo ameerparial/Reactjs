f8233204ab0fdf14dd06c468e54fd0809b4f44cd
ameerparial <khuhroameerhamza4@gmail.com>
2023-05-05 12:35:30 +0500

    Added functionality to buy the things and calculate the total bill.

--- src/Components/RenderList.js
+++ src/Components/RenderList.js
@@ -1,16 +1,34 @@
 
 function ListItem({list}){
-    const content = list.map((value)=>{
-        return <li>{value[0]}
+    let totalBill = 0;
+    
+    const content = list.map((value, index)=>{
+        return <li key={'item'+index}>{value['name']}
             <ul>
-                <li>{`Cost Rs. ${value[1]}`}</li>
+                <li key={'sub-item'+index}>{`Cost Rs. ${value['price']}`}</li>
             </ul>
+            <label>Add item to cart: <input type="checkbox" id={index} onChange={(e)=>{
+                const index = e.target.id;
+                if(e.target.checked)
+                    totalBill +=list[index].price;
+                else
+                    totalBill -=list[index].price;
+                
+                // e.target.checked?list[index].sold=true:list[index].sold=false;
+            }}/></label>
         </li>
     });
     return (
-        <ul>
-            {content}
-        </ul>
+        <>
+            <ul>
+                {content}
+            </ul>
+            <button onClick={()=>{
+                console.log({totalBill});
+                
+
+            }}>Calculate Bill</button>
+        </>
     );
 
 
