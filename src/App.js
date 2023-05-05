a67251a264dd9c6cbc3ae2425f56874dc3df3352
ameerparial <khuhroameerhamza4@gmail.com>
2023-05-05 11:00:50 +0500

    Added the render list component to the app.js -main JS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import MyButton from './Components/MyButton';
+import ListItem from './Components/RenderList';
 import { useState } from 'react';
+
 
 function App() {
   const [count, setCount] = useState(0);
+  const list = ['Apple', 'Banana', 'Mango', 'Pine Apple', 'Orange'];
   function increaseValue(){
     if(count<50){
       setCount(count+1);     
@@ -19,9 +22,16 @@ function App() {
     count>=1?setCount(count-1):alert('You reached the minimum');
   }
   return (
-    <div className='App'>
-      <MyButton count={count} increaseCount={increaseValue} resetCount={resetValue} decreaseCount={decreaseValue}/>
-    </div>
+    <>
+      <h2 className='Task'>Task 1 Counter</h2>
+      <div className='Buttons'>
+        <MyButton count={count} increaseCount={increaseValue} resetCount={resetValue} decreaseCount={decreaseValue}/>
+      </div>
+      <hr></hr>
+      <h2 className='Task'>Task 2 Render List</h2>
+      <ListItem list={list}/>
+    </>
+
   );
 }
 
