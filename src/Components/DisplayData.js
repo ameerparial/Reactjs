8e238c009585f6e579c9f21019991f3e429bb977
ameerparial <khuhroameerhamza4@gmail.com>
2023-05-06 00:31:11 +0500

    Add the function that calls on button click to change the state i.e. profile image using setState()

--- src/Components/DisplayData.js
+++ src/Components/DisplayData.js
@@ -1,16 +1,26 @@
 import React from "react";
+import next from '../Images/ameerhamza.jpeg';
 
 class DisplayData extends React.Component{
+    
     constructor(props){
         super(props);
         this.state = props.user;
         console.log(this.state);
     }
+
+    changeImage = ()=>{
+        //First way
+        // this.setState({...this.state, image:next});
+        //Second way
+        this.setState((data)=>data.image=next);
+    }
     render(){
         return <>
         
         <p>{this.state.name}</p>
-        <img className="profile" src={this.state.image} style={{borderRadius:"100%", width:"60px", height:"60px"}} alt="Not Found"/>
+        <img src={this.state.image} style={{borderRadius:"100%", width:"60px", height:"60px", display:"block"}} alt="Not Found"/>
+        <button onClick={this.changeImage}>Change Image</button>
         </>
 
     }
